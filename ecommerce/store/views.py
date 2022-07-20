ccad4d054a774544155a55a81e12146b3a361873
Tasnim-Talaat <tasnim.talaat23@gmail.com>
2022-07-20 17:48:59 +0200

    tasnim

---
+++ ecommerce/store/views.py
@@ -0,0 +1,25 @@
+
+
+
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
+from  .models import * 
+
+def home (request):
+    return render(request , "store/index.html")
+
+def collections(request):
+    category = Category.objects.filter(status =0)
+    context =   {'category' : category}
+    return render(request , "store/collections.html" , context)
+
+
+def collectionsview (request , slug):
+    if(Category.objects.filter(slug=slug , status = 0)):
+        products = Product.objects.filter(category__slug = slug)
+        category = Category.objects.filter(slug=slug).first()
+        context = {'products' : products , 'category':category}
+        return render(request , 'store/products/index.html' , context)
+    else:
+      messages.warning(request , 'No such category found')
+      return redirect('collections')
