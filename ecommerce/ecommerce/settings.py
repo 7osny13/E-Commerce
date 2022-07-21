7fc4cd186b9b0f964ad04b3a70efee36c2bf154f
Tasnim-Talaat <tasnim.talaat23@gmail.com>
2022-07-21 15:56:35 +0200

    tasnim

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'finall',
+        'NAME': 'finaltest',
         'HOST': 'localhost',
         'USER': 'postgres',
         'PASSWORD': '2023',
